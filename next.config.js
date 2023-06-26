ecd436ce6045ead79e6ef0ed19fd8da2bce602b4
Graph and Co <contact@graphandco.com>
2023-06-26 19:11:48 +0200

    Updated next authorized images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["images.pexels.com", "fastly.picsum.photos", "cloudinary.com"],
+        domains: ["images.pexels.com", "fastly.picsum.photos", "cloudinary.com", "next13-cheat-sheet.vercel.app"],
     },
 };
 
